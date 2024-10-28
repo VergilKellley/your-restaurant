29f726e75bb097557493f5a0fdbda54067423da1
VergilKellley <verg_1@yahoo.com>
2024-10-27 20:06:21 -0500

    first commit

---
+++ backend/delete_menu_item.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require 'db.php';
+
+if (isset($_POST['submit_delete_menu_item']))
+ {
+
+
+    $delete_menu_item_info_id = filter_var($_POST['submit_delete_menu_item'], FILTER_SANITIZE_NUMBER_INT);
+
+    $delete_menu_item_query = "DELETE FROM menu_item WHERE id = '$delete_menu_item_info_id'";
+
+    $delete_menu_item_result = mysqli_query($conn, $delete_menu_item_query);
+
+    if($delete_menu_item_result) {
+        header("Location: ../edit_menu_item_info.php");
+        exit();
+    } else {
+        echo "Something went wrong.";
+    }
+}
