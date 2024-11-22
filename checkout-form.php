ef11167badf0a46fa5f416d0ed2f55698710d1a1
VergilKellley <verg_1@yahoo.com>
2024-11-21 20:16:34 -0600

    first commit

---
+++ checkout-form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="checkout.php
+    " method="post">
+               <p>T-shirts</p>
+               <p><strong>25.00</strong></p>
+               <button>pay now!</button>
+    </form>
+</body>
+</html>
