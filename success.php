c14cd6de1f8bdc7fa1d5444905a8b29b4c61f6fe
VergilKellley <verg_1@yahoo.com>
2024-11-21 20:15:39 -0600

    first commit

---
+++ success.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Successful Payment</title>
+</head>
+<body>
+    <div style="display: flex; flex-direction:column; justify-content: center; align-items: center ;">
+        <h1 style="font-size: 30px; color: green;">Thank you for your payment!</h1>
+        <br>
+        <div><a href="index">back to home page </a></div>
+    </div>
+</body>
+</html>
