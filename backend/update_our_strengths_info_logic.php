e51032439789136e7b5b1cdf029a6fd5b819f01c
VergilKellley <verg_1@yahoo.com>
2024-11-25 20:55:43 -0600

    added image and images description and code

--- backend/update_our_strengths_info_logic.php
+++ backend/update_our_strengths_info_logic.php
@@ -295,13 +295,88 @@ if (($_SERVER["REQUEST_METHOD"] != "POST") || (!isset($_SESSION["user_id"]))) {
         $our_strengths_info_result = mysqli_query($conn, $update_our_strengths_info_query);
 
         if(!mysqli_errno($conn)) {
-            header('location: ../edit_our_strengths.php');
+            header('location: ../edit_our_strengths_info');
             die();
             } else {
-                header('location: ../edit_our_strengths.php');
+                header('location: ../edit_our_strengths_info');
                 die();
             }
         // }
     }
 // }
+
+
+// OUR STRENGTHS RIGHT IMG
+if (($_SERVER["REQUEST_METHOD"] != "POST") || (!isset($_SESSION["user_id"])) || (empty($_FILES["image"]["name"])) && isset($_POST["submit_update_our_strengths_right_img"])) {
+    //8056477249 voice mail
+    $_SESSION["empty_right_image"] = 'image NOT changed... click edit and choose an image.';
+    header('location: ../edit_our_strengths_info#edit-right-img');
+       
+} elseif (($_SERVER["REQUEST_METHOD"] == "POST") && (isset($_SESSION["user_id"])) && (!empty($_FILES["image"]["name"])) && isset($_POST["submit_update_our_strengths_right_img"])) {
+    
+    unset ($_SESSION['empty_right_image']);
+
+    $our_strengths_info_id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
+    $fileName = $_FILES["image"]["name"];
+    $our_strengths_right_img_desc = filter_var($_POST['our_strengths_right_img_desc'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    // $news_article_text = htmlspecialchars($_POST["news_article_text"]
+    // , ENT_QUOTES);
+
+    $ext = pathinfo($fileName, PATHINFO_EXTENSION);
+    $allowedTypes = array("jpeg", "jpg", "png", "gif", "webp");
+    $tempName = $_FILES["image"]["tmp_name"];
+    $targetPath = "../assets/images/".$fileName;
+    if(in_array($ext, $allowedTypes)) {
+        if (move_uploaded_file($tempName, $targetPath)) {
+    
+
+        $update_our_strengths_info_query = "UPDATE our_strengths SET our_strengths_right_img = '$fileName',  our_strengths_right_img_desc = '$our_strengths_right_img_desc' WHERE id = $our_strengths_info_id;";
+
+        $our_strengths_info_result = mysqli_query($conn, $update_our_strengths_info_query);
+
+        if(!mysqli_errno($conn)) {
+            header('location: ../edit_our_strengths_info#edit-right-img');
+            die();
+            } 
+        }
+    }
+}
+
+// OUR STRENGTHS LEFT IMG
+if (($_SERVER["REQUEST_METHOD"] != "POST") || (!isset($_SESSION["user_id"])) || (empty($_FILES["image"]["name"])) && isset($_POST["submit_update_our_strengths_left_img"])) {
+    //8056477249 voice mail
+    $_SESSION["empty_left_image"] = 'image NOT changed... click edit and choose an image.';
+    header('location: ../edit_our_strengths_info#edit-left-img');
+       
+} elseif (($_SERVER["REQUEST_METHOD"] == "POST") && (isset($_SESSION["user_id"])) && (!empty($_FILES["image"]["name"])) && isset($_POST["submit_update_our_strengths_left_img"])) {
+    
+    unset ($_SESSION['empty_left_image']);
+
+    $our_strengths_info_id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
+    $fileName = $_FILES["image"]["name"];
+    $our_strengths_left_img_desc = filter_var($_POST['our_strengths_left_img_desc'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    // $news_article_text = htmlspecialchars($_POST["news_article_text"]
+    // , ENT_QUOTES);
+
+    $ext = pathinfo($fileName, PATHINFO_EXTENSION);
+    $allowedTypes = array("jpeg", "jpg", "png", "gif", "webp");
+    $tempName = $_FILES["image"]["tmp_name"];
+    $targetPath = "../assets/images/".$fileName;
+    if(in_array($ext, $allowedTypes)) {
+        if (move_uploaded_file($tempName, $targetPath)) {
+    
+
+        $update_our_strengths_info_query = "UPDATE our_strengths SET our_strengths_left_img = '$fileName',  our_strengths_left_img_desc = '$our_strengths_left_img_desc' WHERE id = $our_strengths_info_id;";
+
+        $our_strengths_info_result = mysqli_query($conn, $update_our_strengths_info_query);
+
+        if(!mysqli_errno($conn)) {
+            header('location: ../edit_our_strengths_info#edit-left-img');
+            die();
+            } 
+        }
+    }
+}
 
