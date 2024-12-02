5680f8ec161d31f650b23dc0a28853315e34c51e
VergilKellley <verg_1@yahoo.com>
2024-12-01 21:11:44 -0600

    added login

--- backend/update_footer_info_logic.php
+++ backend/update_footer_info_logic.php
@@ -7,8 +7,8 @@ if (($_SERVER["REQUEST_METHOD"] != "POST") || (!isset($_SESSION["user_id"]))) {
 } elseif ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit_update_footer_titles_info'])) {
 
     $footer_info_id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
-    $footer_facebook = filter_var($_POST['footer_facebook'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $footer_instagram = filter_var($_POST['footer_instagram'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $footer_main_title = filter_var($_POST['footer_main_title'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $footer_sub_title = filter_var($_POST['footer_sub_title'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 
     // $news_article_text = htmlspecialchars($_POST["news_article_text"]
@@ -16,7 +16,7 @@ if (($_SERVER["REQUEST_METHOD"] != "POST") || (!isset($_SESSION["user_id"]))) {
 
     // $fileName = $_FILES["image"]["name"];
 
-        $update_footer_info_query = "UPDATE footer SET footer_facebook = '$footer_facebook', footer_instagram = '$footer_instagram' WHERE id = $footer_info_id;";
+        $update_footer_info_query = "UPDATE footer SET footer_main_title = '$footer_main_title', footer_sub_title = '$footer_sub_title' WHERE id = $footer_info_id;";
 
         $footer_info_result = mysqli_query($conn, $update_footer_info_query);
 
@@ -51,10 +51,10 @@ if (($_SERVER["REQUEST_METHOD"] != "POST") || (!isset($_SESSION["user_id"]))) {
         $footer_info_result = mysqli_query($conn, $update_footer_info_query);
 
         if(!mysqli_errno($conn)) {
-               header ('location: ../edit_footer_social_media_info.php');
+               header ('location: ../edit_footer_info.php');
             die();
             } else {
-                header ('location: ../edit_footer_social_media_info.php');
+                header ('location: ../edit_footer_info.php');
                 die();
      }
 }
