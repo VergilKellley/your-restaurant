fe5a72eb96c32e15e472ba52fa7529095c60c33a
VergilKellley <verg_1@yahoo.com>
2024-12-03 20:44:18 -0600

    added php variables

--- backend/Update_our_strengths_info_4.php
+++ backend/Update_our_strengths_info_4.php
@@ -34,8 +34,10 @@ if (($_SERVER["REQUEST_METHOD"] != "GET") || (!isset($_SESSION["user_id"]))) {
 
         <section style='padding: 20px; width:100vw; display:flex; justify-content: center; align-content:center'>
             <div style='width: 100%; max-width: 525px;'>
-                <a style='font-size:18px' href="../edit_our_strengths_info.php"> Back</a>
-                <h2 style='font-size:22px; padding: 30px;'>edit title & text 4</h2>
+                <a style='font-size:18px' href="../edit_our_strengths_info#edit-info-4"> Back</a>
+                <br>
+                <br>
+                <!-- <h2 style='font-size:22px; padding: 30px;'>edit title & text 4</h2> -->
                 <form style=" display:flex; flex-direction:column; gap 1rem; width:100%"
                     action="update_our_strengths_info_logic.php" enctype="multipart/form-data" method="POST">
 
