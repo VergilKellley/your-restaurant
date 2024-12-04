3ddfe0c5de24e9b3b7e89ae40982ccae97c01135
VergilKellley <verg_1@yahoo.com>
2024-12-03 20:45:03 -0600

    added multi-product functionality

--- assets/js/shopping-cart.js
+++ assets/js/shopping-cart.js
@@ -2,7 +2,7 @@ class CartItem{
     constructor(name, desc, img, price){
         this.name = name
         this.desc = desc
-        this.img=img
+        this.img = img
         this.price = price
         this.quantity = 1
    }
@@ -57,20 +57,60 @@ const cartIcon = document.querySelector('.fa-cart-arrow-down')
 const wholeCartWindow = document.querySelector('.whole-cart-window')
 wholeCartWindow.inWindow = 0
 const addToCartBtns = document.querySelectorAll('.add-to-cart-btn')
+
+for (let i = 0; i < addToCartBtns.length; i++) {
+    const selectedMenuItems = addToCartBtns[i];
+    // const MenuItems = selectedMenuItems.textContent;
+    console.log(selectedMenuItems);
+}
+
+// const menuArray = Array.from(addToCartBtns);
+// console.log("my array:", menuArray)
+
 addToCartBtns.forEach( (btn)=>{
+
     btn.addEventListener('click', addItemFunction)
 }  )
 
+// const menuItems = document.querySelectorAll('.menu-items')
+
+// menuItems.forEach( e => {
+//     console.log(e)
+// })
+
+// const elements = document.querySelectorAll('data-id');
+
+// for (const element of elements) {
+//   const dataId = element.dataset.id;
+//   // Do something with the dataId
+//   console.log("new data ids: ", dataId); 
+// }
+
+
+var el;
+// var prefix = 'elementId';
+for(var i = 0; el = document.getElementById(menus + i); i++) {
+    console.log("the el:", el)
+//   if(condition) {
+//     el.style.display = '';
+//   }
+}
+
 function addItemFunction(e){
     const id = e.target.parentElement.parentElement.parentElement.getAttribute("data-id")
+    console.log("my id: ", id)
     const img = e.target.parentElement.parentElement.previousElementSibling.src
     const name = e.target.parentElement.previousElementSibling.textContent
     const desc = e.target.parentElement.children[0].textContent
     let price = e.target.parentElement.children[1].textContent
-    price = price.replace("Price: $", '')
+    price = price.replace("Price: " , '')
+    // price = price.replace("Price: $", '')
     const item = new CartItem(name, desc, img, price)
     LocalCart.addItemToLocalCart(id, item)
- console.log(price)
+ console.log("the price is: ", price)
+ console.log("the menu item is: ", name)
+ console.log("the menu item description is: ", desc)
+ console.log("the menu item image: ", img)
 }
 
 
@@ -101,31 +141,73 @@ cartIcon.addEventListener('mouseleave', ()=>{
 
 function updateCartUI(){
     const cartWrapper = document.querySelector('.cart-wrapper')
+    console.log("cart wrapper: ", cartWrapper)
     cartWrapper.innerHTML=""
     const items = LocalCart.getLocalCartItems()
     if(items === null) return
     let count = 0
     let total = 0
+
+    // console.log("new 3 price: ", price);
     for(const [key, value] of items.entries()){
         const cartItem = document.createElement('div')
         cartItem.classList.add('cart-item')
-        let price = value.price*value.quantity
+        console.log("my value: ", value)
+        let price = value.price*value.quantity        
         price = Math.round(price*100)/100
         count+=1
         total += price
         total = Math.round(total*100)/100
+        
+
+        ////// start send cart items to stripe checkout page
+
+        let itemName = value.name;
+        let itemDesc = value.desc;
+        let itemQuantity = value.quantity;
+        let itemPrice = price;
+
+        let purchasedItems = {
+            "itemName": itemName,
+            "itemDesc": itemDesc,
+            "itemQuantity": itemQuantity,
+            "price": price
+        }
+
+        let jsonpurchasedItems = JSON.stringify(purchasedItems);
+        fetch('/your-restaurant/test_fetch.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            "body": JSON.stringify(purchasedItems)
+        }).then(function(response){
+            return response.json();
+        }).then(function(data){
+            JSON.stringify(data)
+
+        }).catch (function (err) {
+            console.log(err);
+        });
+
+        // <h3 style="color:black">${value.name}</h3>
+        // <p style="color:black">${value.desc}
+        // <span style="color:black" class="quantity">Quantity: ${value.quantity}</span>
+        // <span style="color:black" class="price">Price: $ ${price  = ((price * 100) / 100).toFixed(2)}</span>
+        // </p>
         cartItem.innerHTML =
         `
         <img src="${value.img}"> 
                        <div class="details">
-                           <h3 style="color:black">${value.name}</h3>
-                           <p style="color:black">${value.desc}
-                            <span style="color:black" class="quantity">Quantity: ${value.quantity}</span>
+                            <input type='text' name='name[]' value='${value.name}'>
+                            <textarea style='overflow-y:scroll; background-color:#d3d3d3; height:100px;padding:5px; border:1px solid black' name="desc[]" id="desc">${value.desc}</textarea>
                             
-                            <span style="color:black" class="price">Price: $ ${price  = (Math.round(price * 100) / 100).toFixed(2)}</span>
-                           </p>
+                            <input style="color:green" class="quantity" type='number' name='quantity[]' value='${value.quantity}'>
+                            
+                            <input style='color:red' class='price' type='text' name='price[]' step="0.01" value='${price  = ((price * 100) / 100).toFixed(2)}'>
                        </div>
                        <div class="cancel"><i class="fas fa-window-close"></i></div>
+
         `
        cartItem.lastElementChild.addEventListener('click', ()=>{
            LocalCart.removeItemFromCart(key)
@@ -133,7 +215,7 @@ function updateCartUI(){
         cartWrapper.append(cartItem)
     } if (count <= 0){
         cartIcon.classList.remove('non-empty')
-        const subtotal = document.querySelector('.subtotal')
+        let subtotal = document.querySelector('.subtotal')
         
         subtotal.innerHTML = `Subtotal: $0.00`
 
@@ -143,11 +225,18 @@ function updateCartUI(){
         root.style.setProperty('--after-content', `"${count}"`)
         const subtotal = document.querySelector('.subtotal')
         // subtotal.innerHTML = `SubTotal: $${total}`
-        subtotal.innerHTML = `Subtotal: $ ${total = (Math.round(total * 100) / 100).toFixed(2)}`
+
+        // Subtotal: $ ${total = (Math.round(total * 100) / 100).toFixed(2)}
+        subtotal.innerHTML = `<input style='color:blue' type='text' name='subtotal' value='Subtotal: $ ${total = (Math.round(total * 100) / 100).toFixed(2)}'>`
+        // console.log("the subtotal for today: ", subtotal);
     }
-    else
+    else {
+        let subtotal = document.querySelector('.subtotal')
+        
     cartIcon.classList.remove('non-empty')
      subtotal.innerHTML = `Subtotals: $0.00`
+    }
+    
 }
 document.addEventListener('DOMContentLoaded', ()=>{updateCartUI()})
     
