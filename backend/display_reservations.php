76d683487806c0f7c51d159076a181ac65b500be
VergilKellley <verg_1@yahoo.com>
2024-12-03 20:39:42 -0600

    added text

--- backend/display_reservations.php
+++ backend/display_reservations.php
@@ -7,7 +7,7 @@ $resultCheck = mysqli_num_rows($result);
 
 if ($resultCheck > 0) {
     while ($row = mysqli_fetch_assoc($result)) {   
-    $main_title = $row['main_title'];
+    $reservation_title = $row['reservation_title'];
     $reservation_text = $row['reservation_text'];
     }
 }
